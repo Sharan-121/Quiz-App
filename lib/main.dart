27005c0b64e266463be103322a10781e8b48114d
Sharan-121 <sharanvignesh123@gmail.com>
2022-01-20 22:30:06 +0530

    Added Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,37 @@ void main() {
 
 // void main() => runApp(MyApp()); // Short hand notation for function with one expression.
 
-class MyApp extends StatelessWidget {
-  // context holds some metadata about the widget
+// This widget could be rebuilt whereas the state could be persistent.
+// State is a generic type.
+// setState forces to re-run build method
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
 
+// Private Class
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0; // To dynamically change questions.
+  var questionSet = [
+    "What's your favorite color?",
+    "What's your favorite animal?",
+  ];
   void answerQuestion() {
-    print("Answer chosen!");
+    setState(() {
+      _questionIndex += 1;
+    });
   }
 
-  @override //decorator
+  // Context holds some metadata about the widget.
+  @override
   Widget build(BuildContext context) {
     // MaterialApp class does a lot of behind the work scenes to display the widgets onto the screen.
     // Home is the core widget.
     // Scaffold widget gives the app a basic design and structure.
+    // We can pass only one widget to body.
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -28,10 +47,11 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text("The Question!"),
+            Text(
+                questionSet[_questionIndex]), // or questionSet.elementAt(index).
             ElevatedButton(
               onPressed:
-                  answerQuestion, // not answerQuestion() coz we need a pointer to the function(name instead of result)
+                  answerQuestion, // Not answerQuestion() coz we need a pointer to the function(name instead of result).
               child: Text("Answer 1"),
             ),
             ElevatedButton(
@@ -41,7 +61,7 @@ class MyApp extends StatelessWidget {
             ElevatedButton(
               onPressed: () {
                 print("Answer chosen!");
-              }, //anonymous function
+              }, //Anonymous Function.
               child: Text("Answer 3"),
             ),
             ElevatedButton(
@@ -49,7 +69,7 @@ class MyApp extends StatelessWidget {
               child: Text("Answer 4"),
             ),
           ],
-        ), // we can pass only one widget to body.
+        ),
       ),
     );
   }
